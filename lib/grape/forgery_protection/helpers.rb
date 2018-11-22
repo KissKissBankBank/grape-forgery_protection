621c1ecb3736116dbf6fdad7530923c72c3dc187
Cyril LEPAGNOT <cyril@lepagnot.fr>
2018-11-22 15:57:04 +0100

    [Refactoring] Some code layout improvements in Helper (#2)

--- lib/grape/forgery_protection/helpers.rb
+++ lib/grape/forgery_protection/helpers.rb
@@ -1,19 +1,6 @@
 module Grape
   module ForgeryProtection
     module Helpers
-      def session
-        env['rack.session']
-      end
-
-      def protect_against_forgery
-        error!('Unauthorized', 401) unless verified_request?
-      end
-
-      def verified_request?
-        !protect_against_forgery? || request.get? || request.head? ||
-          form_authenticity_token == csrf_token_from_headers
-      end
-
       def csrf_token_from_headers
         request.headers['X-CSRF-Token'].presence ||
           request.headers['X-Csrf-Token']
@@ -23,13 +10,31 @@ module Grape
         session[:_csrf_token] ||= SecureRandom.base64(32)
       end
 
+      def protect_against_forgery
+        return if verified_request?
+
+        error!('Unauthorized', 401)
+      end
+
       def protect_against_forgery?
         allow_forgery_protection = Rails
           .configuration
           .action_controller
           .allow_forgery_protection
 
-        allow_forgery_protection.nil? || allow_forgery_protection
+        allow_forgery_protection.nil? ||
+          allow_forgery_protection
+      end
+
+      def session
+        env['rack.session']
+      end
+
+      def verified_request?
+        !protect_against_forgery? ||
+          request.get? ||
+          request.head? ||
+          form_authenticity_token == csrf_token_from_headers
       end
     end
   end
